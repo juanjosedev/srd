0c785eae6e838ddeca026248f67dcb6e1ce02cb3
juanjosedev <programadorsito98@gmail.com>
2019-11-01 00:29:31 -0500

    Otras dependencias

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router/router'
+import BootstrapVue from 'bootstrap-vue'
+import './assets/app.css'
+import './assets/app.js'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
